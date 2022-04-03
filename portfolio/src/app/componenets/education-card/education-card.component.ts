d9b6847f5ebb9eefb1dbf5e654ae7290157544a8
Levon <LevonSwenson@gmail.com>
2022-04-03 15:41:33 -0500

    Updated education/experience sections

--- portfolio/src/app/componenets/education-card/education-card.component.ts
+++ portfolio/src/app/componenets/education-card/education-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-education-card',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class EducationCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input() Title: string = "";
+  @Input() ImgSrc: string = "";
+  @Input() description: string = "";
 
   ngOnInit(): void {
   }
